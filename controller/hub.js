d04b9b392f04dfb5572f3be221cc86ba94d1d254
ceepu8 <caophuonguyen132002@gmail.com>
2023-05-10 17:08:09 +0700

    add get hub api

--- controller/hub.js
+++ controller/hub.js
@@ -1,6 +1,21 @@
 require('dotenv').config();
 const Hub = require('../models/Hub');
 
+const getHub = async (req, res) => {
+  try {
+    let hub = await Hub.find();
+
+    if (hub.length > 0) {
+      res.status(200).json({ success: true, message: 'Success', data: hub });
+    } else {
+      res.status(404).json({ success: true, message: 'There is not any products' });
+    }
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ success: false, message: 'Internal server error' });
+  }
+};
+
 const createHub = async (req, res) => {
   const { name, address } = req.body;
 
@@ -29,4 +44,4 @@ const createHub = async (req, res) => {
   }
 };
 
-module.exports = { createHub };
+module.exports = { createHub, getHub };
